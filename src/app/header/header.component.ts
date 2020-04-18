d63b377d9f925ea9e939533281453c38fa0ff996
William Devine <william@williamdevine.ca>
2020-04-18 15:08:01 -0300

    Initial mobile menu.

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,17 +1,50 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
-  selector: 'app-header',
-  templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+    selector: 'app-header',
+    templateUrl: './header.component.html',
+    styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() { }
+    constructor() {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+        $('#mobileHeader #navHam').click(() => {
+            // Expand Menu
+            $('#mobileHeader #mobileCont').addClass('mobileHeaderDivHover');
 
+            // Show Links
+            $('#mobileHeader #mobileCont .links').css('display', 'flex');
+
+            // Change Icon
+            $('#mobileHeader #mobileCont .navIcons').css({width: 'unset', height: 'unset'});
+            $('#mobileHeader #mobileCont #nav1').hide();
+            $('#mobileHeader #mobileCont #navX').css('display', 'flex');
+            $('#mobileHeader #mobileCont #nav2').addClass('nav2Add');
+
+        });
+
+
+        $('#mobileHeader #mobileCont .navIcons #navX, #mobileHeader #mobileCont .links .link').click(() => {
+            console.log('here');
+            // Retract Menu
+            $('#mobileHeader #mobileCont').attr('class', '');
+
+            // Hide Links
+            $('#mobileHeader #mobileCont .links').css('display', 'none');
+
+            // Change Icon
+            $('#mobileHeader #mobileCont .navIcons').css({width: '100vw', 'margin-bottom': '20vh'});
+            $('#mobileHeader #mobileCont #nav1').css('display', 'flex');
+            $('#mobileHeader #mobileCont #navX').hide();
+            $('#mobileHeader #mobileCont #nav2').removeClass('nav2Add');
+
+        });
+
+    }
 
 
 }
